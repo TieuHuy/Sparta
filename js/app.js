8fa1463cff97392ff44dbea921db275146936d01
Kristo Kiis <kristo@efley.ee>
2013-10-23 20:46:39 +0300

    phonegap 3.0

--- js/app.js
+++ js/app.js
@@ -1888,7 +1888,7 @@ var app = {
 			
 			if (type != 'bill') {
 			
-				iabRef = window.open('http://projects.efley.ee/fitnessapp/admin/payment.php?order=' + order_id + '&bank=' + type, '_blank', 'location=yes');
+				iabRef = window.open('http://fitnessmobile.ee/payment.php?order=' + order_id + '&bank=' + type, '_blank', 'location=yes');
 				iabRef.addEventListener('loadstart', function() {
 					//console.log('started');
 					//if()
@@ -1896,7 +1896,7 @@ var app = {
 				iabRef.addEventListener('loadstop', function(event) {
 				
 					//console.log(event.url);
-					if(event.url == 'http://projects.efley.ee/fitnessapp/admin/payment.php?order=' + order_id + '&bank=' + type + '&success=1') {
+					if(event.url == 'http://fitnessmobile.ee/payment.php?order=' + order_id + '&bank=' + type + '&success=1') {
 						iabRef.close();
 						
 						$('#buyoverlay').find('.checkoutcontent').hide();
@@ -1905,7 +1905,7 @@ var app = {
 						$('#buyoverlay').find('.backbtn').hide();
 					}
 					
-					if(event.url == 'http://projects.efley.ee/fitnessapp/admin/payment.php?order=' + order_id + '&bank=' + type + '&error=1') {
+					if(event.url == 'http://fitnessmobile.ee/payment.php?order=' + order_id + '&bank=' + type + '&error=1') {
 						iabRef.close();
 						
 						$('#buyoverlay').find('.checkoutcontent').hide();
